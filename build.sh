8ef56a66475200f5b0019faee68ca83f6015d020
Timothy Klim <klimtimothy@gmail.com>
2016-12-09 18:41:14 +0300

    feat: upgrade scala to 2.12

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function build {
-  IMAGE="fcomb/$1"
+  VERSION=${2:+":$2"}
+  IMAGE="fcomb/$1$VERSION"
   docker build ${BUILD_OPTS} --force-rm --no-cache -t ${IMAGE} ${ROOT}/$1
   docker push ${IMAGE}
 }
 
-time build "jdk8-sbt-alpine"
-time build "jre8-sbt-alpine"
+time build "jdk8-sbt-alpine" "2.12"
+time build "jre8-sbt-alpine" "2.12"
